d07ff058b96a669b0dc172ab56043bbb3bae6239
Balin Consuela <bconsuel@at-i1.21-school.ru>
2020-01-08 16:01:56 +0300

    fixed get_token, its fully functional now

--- myShell/get_token.c
+++ myShell/get_token.c
@@ -1,42 +1,89 @@
-size_t	get_pos(const char *s1, const char *s2)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_token.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bconsuel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/08 12:30:46 by bconsuel          #+#    #+#             */
+/*   Updated: 2020/01/08 15:38:16 by bconsuel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*
+** Returns an index of the first character of s1
+** that is not in s2.
+*/
+
+size_t	get_npos(const char *s1, const char *s2)
 {
-	int	i;
+	size_t		i;
 
 	i = 0;
-	while (s1)
+	while (s1[i] && ft_strchr(s2, s1[i]))
+		i++;
+	return (i);
+}
+
+/*
+** Returns an index of the first character of s1
+** that is in s2.
+*/
+
+size_t	get_pos(const char *s1, const char *s2)
+{
+	size_t		i;
+	size_t		j;
+
+	i = 0;
+	while (s1[i])
 	{
-		while (s2)
+		j = 0;
+		while (s2[j])
 		{
-			if (s1 == s2)
-				return i;
-			s2++;
+			if (s2[j] == s1[i])
+				return (i);
+			j++;
 		}
-		s1++;
 		i++;
 	}
-	return i;
+	return (i);
 }
+
+/*
+** Splits a string s into tokens pointed at by delim.
+** Returns a pointer to the next token in a string if NULL
+** is passed after passing said string in the first iteration
+** of the function call.
+*/
 
 char	*get_token(char *s, const char *delim)
 {
 	static char	*save;
-	char	*end;
-	
+	char		*end;
+
 	if (s == NULL)
-		s = *save;
-	if (*s == '#0')
+		s = save;
+	if (*s == '\0')
 	{
-		*save = s;
-		return NULL;
+		save = s;
+		return (NULL);
+	}
+	s += get_npos(s, delim);
+	if (*s == '\0')
+	{
+		save = s;
+		return (NULL);
 	}
 	end = s + get_pos(s, delim);
-//	end = ft_strcmp(s, delim);
-	if (*end == '#0')
+	if (*end == '\0')
 	{
-		*save = end;
-		return s;
+		save = end;
+		return (s);
 	}
-	*end = '#0';
-	*save = end + 1;
-	return s;
+	*end = '\0';
+	save = end + 1;
+	return (s);
 }
