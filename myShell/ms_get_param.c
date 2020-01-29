49b3d519714d0eaf4363fa5e99f79b251cf95290
Balin Consuela <bconsuel@at-i1.21-school.ru>
2020-01-29 17:43:32 +0300

    working on parameter expansion

--- myShell/ms_get_param.c
+++ myShell/ms_get_param.c
@@ -6,28 +6,30 @@
 /*   By: bconsuel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 12:48:55 by bconsuel          #+#    #+#             */
-/*   Updated: 2020/01/29 13:43:57 by bconsuel         ###   ########.fr       */
+/*   Updated: 2020/01/29 17:42:35 by bconsuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void		expand_brace(char *str)
+{
+}
+
 char		*ms_get_param(char *str, char **environ)
 {
 	int		start;
 	int		end;
-	char	*tmp;
+	char	*temp;
 
 	start = 0;
 	end = ft_strlen(str);
-	while (*str)
-	{
-		if (*str == '$' || *str == '{')
-			start++;
-		if (*str == '}')
-			end--;
+	if (*str == '$')
 		str++;
-	}
-	tmp = ft_strsub(str, start, end - start + 1);
+	if (*str == '{')
+		temp = expand_brace(str);
+	else
+		temp = expand_arg(str);
+printf("testing: %s\n", str);
 	return (str);
 }
